7961df604bd1384a9a2f54baa6400ef42484897e
milkra160 <290503@student.pwr.edu.pl>
2026-05-25 20:53:58 +0200

    komentarze

--- src/morderca_krow.py
+++ src/morderca_krow.py
@@ -4,7 +4,7 @@ from src.krowa import Krowa
 
 class Morderca(Zwierze):
     def __init__(self,imie: str, wiek: int, czy_zyje: bool):
-        #Morderca ma szanse 15% na pojawienie sie by zbalansowac przebieg symulacji
+        #Morderca ma szanse 15% na pojawienie sie, by zbalansowac przebieg symulacji
         super().__init__(imie, wiek, czy_zyje)
         self.szansa_pojawienia_sie_drapieznika: float = 0.15
 

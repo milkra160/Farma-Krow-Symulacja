74b6e6157fee6d782999fadeaecdbbd0c5f5c18b
milkra160 <290503@student.pwr.edu.pl>
2026-05-25 20:11:58 +0200

    poprawa testu i uaktualnienie pytest

--- tests/test_zwierzeta.py
+++ tests/test_zwierzeta.py
@@ -4,3 +4,8 @@ def test_zwierze_starzeje_sie_gdy_zyje():
     zwierze = Zwierze("Marek",5,True)
     zwierze.starzej_sie()
     assert zwierze.wiek == 6
+
+def test_zwierze_nie_starzeje_sie_gdy_jest_martwe():
+    zwierze = Zwierze("Czarek",8,False)
+    zwierze.starzej_sie()
+    assert zwierze.wiek == 8

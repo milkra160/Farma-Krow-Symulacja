69c9ba543f57b6841d6b1226f671391910952e61
milkra160 <290503@student.pwr.edu.pl>
2026-05-25 20:52:54 +0200

    komentarze

--- src/krowa.py
+++ src/krowa.py
@@ -5,6 +5,8 @@ class Krowa(Zwierze):
     def __init__(self, imie: str, wiek: int, czy_zyje: bool):
         super().__init__(imie, wiek, czy_zyje)
 
+        #Ustawienie poziomow glodu i stalych by zbalansowac przebieg symulacji
+
         self.poziom_glodu: int = 0
         self.max_dni_glodowania: int = 3
         self.dni_glodowania_z_rzedu: int = 0
--- src/morderca_krow.py
+++ src/morderca_krow.py
@@ -4,8 +4,10 @@ from src.krowa import Krowa
 
 class Morderca(Zwierze):
     def __init__(self,imie: str, wiek: int, czy_zyje: bool):
+        #Morderca ma szanse 15% na pojawienie sie by zbalansowac przebieg symulacji
         super().__init__(imie, wiek, czy_zyje)
         self.szansa_pojawienia_sie_drapieznika: float = 0.15
+
 
 
 

280b4077864857727f562c38f79f8650da16373b
peterman1337 <piotrek.tarlowski@gmail.com>
2026-05-27 02:18:15 +0200

    Klasa Finanse
    Dodałem klasę finanse, odpowiedzialną za kalkulowanie finansów farmy. Dane przechowywane są w słowniku, by umożliwić ich łatwe wyświetlanie i schludne przechowywanie. Dodatkowo Dzień i aktualny stan konta sa przechowywane w historii

---
+++ src/finanse.py
@@ -0,0 +1,27 @@
+
+class Finanse:
+    def __init__(self, budzet_start: float):
+        self.budzet = budzet_start
+        self.historia = []
+
+    def rozlicz_dzien(self, przychod: float, koszt: float, dzien: int):
+        bilans = przychod - koszt
+        self.budzet += bilans
+
+        #słownik służący do wyświetlania finansów pod koniec dnia
+        stan_finansow = {
+            "budzet": float(self.budzet),
+            "przychod": float(przychod),
+            "koszt": float(koszt),
+            "blians": float(bilans),
+            "bankrut": self.czy_bankrut()
+        }
+
+        self.historia.append({
+            "dzien": dzien,
+            "stan": stan_finansow
+        })
+        return stan_finansow
+
+    def czy_bankrut(self) -> bool:
+        return self.budzet <= 0
